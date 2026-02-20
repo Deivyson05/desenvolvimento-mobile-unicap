965cf067e80e68a8eb330f6470c16f6eb64288eb
Deivyson Ricardo Silva dos Santos <131066052+Deivyson05@users.noreply.github.com>
2026-02-20 23:07:19 +0000

    aula-02

--- aula-01/app/(tabs)/index.tsx
+++ aula-01/app/(tabs)/index.tsx
@@ -1,5 +1,5 @@
 import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
+import { Platform, StyleSheet, View, Text } from 'react-native';
 
 import { HelloWave } from '@/components/hello-wave';
 import ParallaxScrollView from '@/components/parallax-scroll-view';
@@ -9,59 +9,12 @@ import { Link } from 'expo-router';
 
 export default function HomeScreen() {
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/nSobrouNada.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Brutal!</ThemedText>
-        <ThemedText>Não Sobra nada pro beta</ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Passo 1: Tente isso</ThemedText>
-        <ThemedText>
-          Se posicione em posição fetal e chore até não aguentar mais.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <Link href="/modal">
-          <Link.Trigger>
-            <ThemedText type="subtitle">Passo 2: Vire um alfa</ThemedText>
-          </Link.Trigger>
-          <Link.Preview />
-          <Link.Menu>
-            <Link.MenuAction title="Action" icon="cube" onPress={() => alert('Action pressed')} />
-            <Link.MenuAction
-              title="Share"
-              icon="square.and.arrow.up"
-              onPress={() => alert('Share pressed')}
-            />
-            <Link.Menu title="More" icon="ellipsis">
-              <Link.MenuAction
-                title="Delete"
-                icon="trash"
-                destructive
-                onPress={() => alert('Delete pressed')}
-              />
-            </Link.Menu>
-          </Link.Menu>
-        </Link>
-
-        <ThemedText>
-          Vá atrás dos seus objetivos e hobbies, não se deixe levar pelas falácias deste mundo.
-        </ThemedText>
-      </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Passo 3: Adquira aura + ego</ThemedText>
-        <ThemedText>
-          Crie uma rotina de treinos e farme muita aura e ganhe mais ego!
-        </ThemedText>
-      </ThemedView>
-    </ParallaxScrollView>
+    <View className='px-6 py-10'>
+      <View>
+        <Text className='font-semibold text-md'>Rua dos Betas 55</Text>
+      </View>
+      <Text className='font-bold text-3xl'>Olá,</Text>
+    </View>
   );
 }
 
